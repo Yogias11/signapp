09c46a9436c6e2b0de6e35e8f2f61cc9094e4a04
Rolly Maulana Awangga <rolly@awang.ga>
2016-10-11 19:30:18 +0700

    add post method

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,13 @@ from cgi import escape
 import signapp
 
 def application(environ, start_response):
+	try:
+		request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+	except (ValueError):
+		request_body_size = 0
+	
 	uri = environ['REQUEST_URI']
+	request_body = environ['wsgi.input'].read(request_body_size)
 
 	cpdt = escape(uri)[1:]
 
@@ -16,12 +22,14 @@ def application(environ, start_response):
 		result =''	
 		for a in sign.getAllSign(data):
 			result=result+str(a)
+	if request_body_size != 0:
+		result = request_body
 	else:
 		result = "OK"
 	hend = sign.getHtmlEnd()
 
 	respon = hbegin + result + hend
-	start_response('200 OK', [('Content-Type', 'text/html')])
+	start_response('200 OK', [('Content-Type', 'text/html'),('Content-Length', str(len(response_body)))])
 	
 	return [respon]
 
