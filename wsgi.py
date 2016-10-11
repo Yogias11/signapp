0fd90cf63bca908fdd463db440f0d19aeb19af8f
Rolly Maulana Awangga <rolly@awang.ga>
2016-10-11 13:42:38 +0700

    get all sign

--- wsgi.py
+++ wsgi.py
@@ -7,9 +7,8 @@ def application(environ, start_response):
 	uri = environ['REQUEST_URI']
 
 	uri = escape(uri)
-	#sign = signapp.Signapp()	
-
-	respon="ok"
+	sign = signapp.Signapp()	
+	respon=sign.getAllsign()
 
 
 	start_response('200 OK', [('Content-Type', 'text/html')])
