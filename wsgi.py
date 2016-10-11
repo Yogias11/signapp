53b7d5d3434a5df37011f83942235af1e3d0dea4
Rolly Maulana Awangga <rolly@awang.ga>
2016-10-11 13:53:28 +0700

    loop for iterate result

--- wsgi.py
+++ wsgi.py
@@ -7,8 +7,10 @@ def application(environ, start_response):
 	uri = environ['REQUEST_URI']
 
 	uri = escape(uri)
-	sign = signapp.Signapp()	
-	respon=sign.getAllSign(uri)
+	sign = signapp.Signapp()
+	respon=''	
+	for a in sign.getAllSign(uri):
+		respon=respon+a
 
 
 	start_response('200 OK', [('Content-Type', 'text/html')])
