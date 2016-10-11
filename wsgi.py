a70972c45c422523dbf2552519805076149970e2
Rolly Maulana Awangga <rolly@awang.ga>
2016-10-11 12:26:19 +0700

    setting mongo

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from cgi import escape
-import config
 import signapp
 
 def application(environ, start_response):
@@ -10,14 +9,7 @@ def application(environ, start_response):
 	uri = escape(uri)
 	sign = signapp.Signapp()	
 
-	if uri == config.uri1:
-		respon=uri		
-		respon=str(respon[0])
-	elif uri == config.gmsuri:
-                respont=uri
-                respon=str(respont[0])
-	else:
-		respon="oke"
+	respon=sign.getAllSign(uri)
 
 
 	start_response('200 OK', [('Content-Type', 'text/html')])
